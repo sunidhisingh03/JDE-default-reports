97e6e975e0e053d2bb044ec818a52e3e53d3377b
sunidhi-jde <sunidhi.kumari@jdecoffee.com>
2026-09-11 12:48:36 +0530

    Create rg

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ module "runbook1" {
   runbook_name            = "AllResources"
   script_path             = "${path.module}/Scripts/all_resources_report.ps1"
   schedule_name           = "schedule1"
-  start_time              = "2025-11-01T09:30:00+05:30"
-  frequency               = "Month"
+  start_time              = "2026-09-12T09:00:00+05:30"
+  frequency               = "Day"
   interval                = 1
   tags                    = var.tags
 }
@@ -28,7 +28,7 @@ module "runbook2" {
   runbook_name            = "BackupReport"
   script_path             = "${path.module}/Scripts/backup_report.ps1"
   schedule_name           = "schedule2"
-  start_time              = "2025-11-01T09:45:00+05:30"
+  start_time              = "2026-09-12T09:40:00+05:30"
   frequency               = "Month"
   interval                = 1
   tags                    = var.tags
