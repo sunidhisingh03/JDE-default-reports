3a643b308c3b3dd57e657c4bea561881345b3ad8
sunidhi-jde <sunidhi.kumari@jdecoffee.com>
2026-09-16 14:34:10 +0530

    create runbooks

--- module/automation_account/output.tf
+++ module/automation_account/output.tf
@@ -1,3 +1,3 @@
-# output "runtime_environment_id" {
-#   value = azurerm_automation_runtime_environment.ps74.id
-# }
+output "runtime_environment_id" {
+  value = azurerm_automation_runtime_environment.ps74.id
+}
